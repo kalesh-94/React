f17b78c3d92a0f8e5658103c9d56e06def6db5de
kalesh-94 <mahajankalesh@gmail.com>
2025-12-25 15:51:13 +0530

    data with arr of objects

---
+++ 02Component/src/data.js
@@ -0,0 +1,78 @@
+  const cardsData = [
+  {
+    title: "Mountain Escape",
+    description: "A peaceful getaway in the mountains with fresh air and nature.",
+    image:
+      "https://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=800&q=60",
+    tag: "Travel",
+  },
+
+  {
+    title: "Modern Workspace",
+    description: "A clean and minimal workspace setup for productivity.",
+    image:
+      "https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format&fit=crop&w=800&q=60",
+    tag: "Design",
+  },
+
+  {
+    title: "Healthy Breakfast",
+    description: "A balanced and energizing morning meal with fruits and oats.",
+    image:
+      "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?auto=format&fit=crop&w=800&q=60",
+    tag: "Food",
+  },
+
+  {
+    title: "Night City Lights",
+    description: "Experience the beauty of the city glowing under the night sky.",
+    image:
+      "https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?auto=format&fit=crop&w=800&q=60",
+    tag: "City",
+  },
+  {
+    title: "Night City Lights",
+    description: "Experience the beauty of the city glowing under the night sky.",
+    image:
+      "https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?auto=format&fit=crop&w=800&q=60",
+    tag: "City",
+  },
+  {
+    title: "Healthy Breakfast",
+    description: "A balanced and energizing morning meal with fruits and oats.",
+    image:
+      "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?auto=format&fit=crop&w=800&q=60",
+    tag: "Food",
+  },
+  {
+    title: "Night City Lights",
+    description: "Experience the beauty of the city glowing under the night sky.",
+    image:
+      "https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?auto=format&fit=crop&w=800&q=60",
+    tag: "City",
+  },
+  {
+    title: "Night City Lights",
+    description: "Experience the beauty of the city glowing under the night sky.",
+    image:
+      "https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?auto=format&fit=crop&w=800&q=60",
+    tag: "City",
+  },
+    {
+    title: "Healthy Breakfast",
+    description: "A balanced and energizing morning meal with fruits and oats.",
+    image:
+      "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?auto=format&fit=crop&w=800&q=60",
+    tag: "Food",
+  },
+
+    {
+    title: "Healthy Breakfast",
+    description: "A balanced and energizing morning meal with fruits and oats.",
+    image:
+      "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?auto=format&fit=crop&w=800&q=60",
+    tag: "Food",
+  },
+];
+
+export default cardsData;
